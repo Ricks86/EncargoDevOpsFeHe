3d6d069499d97d0b952bb80d683e55efeae482d8
Ricks86 <ju.fernandeza@duocuc.cl>
2026-04-11 21:31:13 -0400

    Estructura del encargo de DevOps

---
+++ src/main/java/com/Encargo/DevOpsFeHe/DevOpsFeHeApplication.java
@@ -0,0 +1,13 @@
+package com.Encargo.DevOpsFeHe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevOpsFeHeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevOpsFeHeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Encargo/DevOpsFeHe/DevOpsFeHeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Encargo.DevOpsFeHe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevOpsFeHeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
